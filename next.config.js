f7da83636ffa045eb88c122972b49ea107b6f25b
Robiul Shagor <mrhshagor@gmail.com>
2024-11-28 03:35:48 +0600

    try to add build

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: 'example.com',
-          // Optional: Specify pathname beginning with a '/'
-          // pathname: '/path/to/images/*',
-        },
-        {
-          protocol: 'https',
           hostname: 'trreb-image.ampre.ca',
-          // pathname can include wildcards such as '*.jpg'
         }
       ],
     }
